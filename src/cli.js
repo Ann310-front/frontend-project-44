c8d214e309668f5cee60ad987128f8cfc0ea4670
Ann310-front <a-konoplina@mail.ru>
2025-10-17 23:46:29 +0300

    done 6 task

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,6 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
     const name = readlineSync.question(' May I have your name?');
     console.log(`Hello,${name}!`);
+    return name;
 };
 export default greeting;
