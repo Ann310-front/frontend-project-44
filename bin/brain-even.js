cb8eefbf3eb47272753bb4234110046179f9d45a
Ann310-front <a-konoplina@mail.ru>
2025-12-02 15:14:46 +0300

    fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 const greeting = () => {
-    const name = readlineSync.question(' May I have your name?');
+    const name = readlineSync.question('May I have your name?');
     console.log(`Hello, ${name}`);
     return name;
 };
