49fd390b612b6d1884165a93e7a23d93fc9f1400
Ann310-front <a-konoplina@mail.ru>
2025-12-12 13:22:52 +0300

    eslint config

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,46 +1,47 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
+import readlineSync from 'readline-sync'
+import greeting from '../src/cli.js'
 
 const calculator = () => {
-  const name = greeting();
-  console.log('What is the result of the expression?');
+  const name = greeting()
+  console.log('What is the result of the expression?')
 
   for (let i = 0; i < 3; i++) {
-    const number1 = Math.floor(Math.random() * 100) + 1;
-    const number2 = Math.floor(Math.random() * 100) + 1;
-    const operators = ['+', '-', '*'];
-    const operator = operators[Math.floor(Math.random() * operators.length)];
-    let correctAnswer;
-    let question;
+    const number1 = Math.floor(Math.random() * 100) + 1
+    const number2 = Math.floor(Math.random() * 100) + 1
+    const operators = ['+', '-', '*']
+    const operator = operators[Math.floor(Math.random() * operators.length)]
+    let correctAnswer
+    let question
 
     switch (operator) {
-    case '+':
-      correctAnswer = number1 + number2;
-      question = `${number1} + ${number2}`;
-      break;
-    case '-':
-      correctAnswer = number1 - number2;
-      question = `${number1} - ${number2}`;
-      break;
-    case '*':
-      correctAnswer = number1 * number2;
-      question = `${number1} * ${number2}`;
-      break;
+      case '+':
+        correctAnswer = number1 + number2
+        question = `${number1} + ${number2}`
+        break
+      case '-':
+        correctAnswer = number1 - number2
+        question = `${number1} - ${number2}`
+        break
+      case '*':
+        correctAnswer = number1 * number2
+        question = `${number1} * ${number2}`
+        break
     }
 
-    console.log(`Question: ${question}`);
-    const userAnswer = parseInt(readlineSync.question('Your answer: '), 10);
+    console.log(`Question: ${question}`)
+    const userAnswer = parseInt(readlineSync.question('Your answer: '), 10)
     if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
+      console.log('Correct!')
+    }
+    else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
     }
   }
 
-  console.log(`Congratulations, ${name}!`);
-};
+  console.log(`Congratulations, ${name}!`)
+}
 
-calculator();
+calculator()
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,35 +1,36 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
+import readlineSync from 'readline-sync'
+import greeting from '../src/cli.js'
 
 const isPrime = (n) => {
   if (n < 2) {
-    return false;
-  }  for (let i = 2; i <= Math.sqrt(n); i++) {
+    return false
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const game = () => {
-  const name = greeting();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const name = greeting()
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
   for (let i = 0; i < 3; i++) {
-    const number = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correct =isPrime(number) ? 'yes' : 'no';
+    const number = Math.floor(Math.random() * 100) + 1
+    console.log(`Question: ${number}`)
+    const answer = readlineSync.question('Your answer: ')
+    const correct = isPrime(number) ? 'yes' : 'no'
     if (answer !== correct) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
     }
 
-    console.log('Correct!');
+    console.log('Correct!')
   }
 
-  console.log(`Congratulations, ${name}!`);
-};
-game();
+  console.log(`Congratulations, ${name}!`)
+}
+game()
